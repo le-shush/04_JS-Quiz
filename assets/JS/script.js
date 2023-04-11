98f71aa9fd4c45da3b9ee01ba1de2a7c9ad7b1b6
LeShush <mycelium.architecture@gmail.com>
2023-04-10 23:06:16 -0600

    Bugfix - clarified route to highscores.html

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -159,7 +159,7 @@ function displayNextQuestion() {
     highScores.sort((a, b) => b.score - a.score);
     highScores.splice(5);
     localStorage.setItem("highScores", JSON.stringify(highScores));
-    window.location.href = "highscores.html";
+    window.location.href = "./assets/highscores.html";
  }
 
  // Event listeners
@@ -167,5 +167,5 @@ function displayNextQuestion() {
  nextBtn.addEventListener("click", displayNextQuestion);
  submitBtn.addEventListener("click", submitResult);
  highScoresLink.addEventListener("click", () => {
-    window.location.href = "highscores.html";
+    window.location.href = "./assets/highscores.html";
  });
